8627447414f5d3bdd9500b5f0ad2d3b63699b56f
Gary Johnson <gjohnson@scottlogic.com>
2016-10-12 14:10:33 +0100

    Added npm scripts to allow tests and builds to be performed on Windows.

---
+++ scripts/run-tests.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+"use strict";
+
+const path = require('path');
+const Mocha = require('mocha');
+const glob = require('glob');
+
+const testGrep = process.env.npm_config_TEST_GREP;
+const testPackage = process.env.npm_config_TEST_ONLY;
+const testSearch = `packages/${testPackage || "*"}/test/*.js`
+
+const mocha = new Mocha();
+
+function getTestFiles() {
+  glob.sync(testSearch).forEach(function(file) {
+    mocha.addFile(file);
+  });
+}
+
+function runTests() {
+  testGrep && mocha.grep(testGrep);
+  mocha.ui('tdd').reporter('dot').timeout(10000).run(function(failures) {
+    process.on('exit', function() {
+      process.exit(failures);
+    });
+  });
+}
+
+getTestFiles();
+runTests();
